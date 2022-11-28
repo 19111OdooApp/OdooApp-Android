8405d1c7c4e29891ba77be744f8aa1d74392efb7
MishkaV <mavorozhtsov_1@edu.hse.ru>
2022-11-28 21:54:32 +0300

    Again fix regex

--- core/utils/src/main/kotlin/odoo/miem/android/core/utils/network/RequestExtensions.kt
+++ core/utils/src/main/kotlin/odoo/miem/android/core/utils/network/RequestExtensions.kt
@@ -1,12 +1,15 @@
 package odoo.miem.android.core.utils.network
 
+import java.util.regex.Pattern
+
 /**
  * For extracting database name
  *
  * Example:
  * https://cool.odoo.ru -> cool
  */
+private val pattern by lazy { Pattern.compile(".+://(\\w+)\\..+") }
+
 internal fun String.getDatabaseFromUrl(): String {
-    val pattern = ".+://(\\w+)\\..+".toRegex()
-    return pattern.find(this)?.groupValues?.get(1) ?: ""
+    return pattern.matcher(this).takeIf { it.find() }?.group(1) ?: ""
 }

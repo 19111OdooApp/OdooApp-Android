7d2903591500b0939ee5590b65e95cb64ad4bef6
LostImagin4tion <danilov6083@outlook.com>
2023-02-18 00:17:41 +0300

    fixed detekt

--- common/uiKitComponents/src/main/kotlin/odoo/miem/android/common/uiKitComponents/cards/BigModuleOutlinedCard.kt
+++ common/uiKitComponents/src/main/kotlin/odoo/miem/android/common/uiKitComponents/cards/BigModuleOutlinedCard.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import odoo.miem.android.common.uiKitComponents.R
 import odoo.miem.android.common.uiKitComponents.utils.animatedGradientBorder
